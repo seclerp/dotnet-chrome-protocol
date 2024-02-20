9e63fd297ed70018afe6f8abbb9f26ea3b28d22c
Andrii Rublov <andrii.rublov@jetbrains.com>
2024-02-20 03:39:33 +0100

    Release 1.0.0
    Also add license entry

---
+++ src/ChromeProtocol.Runtime/Messaging/Extensions/ProtocolClientExtensions.cs
@@ -0,0 +1,78 @@
+﻿using ChromeProtocol.Core;
+
+namespace ChromeProtocol.Runtime.Messaging.Extensions;
+
+public static class ProtocolClientExtensions
+{
+  public static IDisposable SubscribeOnceAsync<TEvent>(this IProtocolClient client, AsyncDomainEventHandler<TEvent> handler)
+    where TEvent : IEvent
+  {
+    var fired = 0;
+    IDisposable? subscription = null;
+    subscription = client.SubscribeAsync<TEvent>(async @event =>
+    {
+      if (Interlocked.CompareExchange(ref fired, 1, 0) != 0)
+        return;
+
+      await handler.Invoke(@event).ConfigureAwait(false);
+
+      subscription?.Dispose();
+    });
+
+    return subscription;
+  }
+
+  public static IDisposable SubscribeOnceSync<TEvent>(this IProtocolClient client, SyncDomainEventHandler<TEvent> handler)
+    where TEvent : IEvent
+  {
+    var fired = 0;
+    IDisposable? subscription = null;
+    subscription = client.SubscribeSync<TEvent>(@event =>
+    {
+      if (Interlocked.CompareExchange(ref fired, 1, 0) != 0)
+        return;
+
+      handler.Invoke(@event);
+
+      subscription?.Dispose();
+    });
+
+    return subscription;
+  }
+
+  public static IDisposable SubscribeOnceAsync<TEvent>(this IScopedProtocolClient client, AsyncDomainEventHandler<TEvent> handler)
+    where TEvent : IEvent
+  {
+    var fired = 0;
+    IDisposable? subscription = null;
+    subscription = client.SubscribeAsync<TEvent>(async @event =>
+    {
+      if (Interlocked.CompareExchange(ref fired, 1, 0) != 0)
+        return;
+
+      await handler.Invoke(@event).ConfigureAwait(false);
+
+      subscription?.Dispose();
+    });
+
+    return subscription;
+  }
+
+  public static IDisposable SubscribeOnceSync<TEvent>(this IScopedProtocolClient client, SyncDomainEventHandler<TEvent> handler)
+    where TEvent : IEvent
+  {
+    var fired = 0;
+    IDisposable? subscription = null;
+    subscription = client.SubscribeSync<TEvent>(@event =>
+    {
+      if (Interlocked.CompareExchange(ref fired, 1, 0) != 0)
+        return;
+
+      handler.Invoke(@event);
+
+      subscription?.Dispose();
+    });
+
+    return subscription;
+  }
+}
