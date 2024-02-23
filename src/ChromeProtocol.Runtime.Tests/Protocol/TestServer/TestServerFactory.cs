918412549bfeeab26bcb1aa13e2ea08a8d9c8e9b
Andrii Rublov <andrii.rublov@jetbrains.com>
2024-02-23 03:14:57 +0100

    Remove fixture from runtime tests

---
+++ src/ChromeProtocol.Runtime.Tests/Protocol/TestServer/TestServerFactory.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+
+namespace ChromeProtocol.Runtime.Tests.Protocol.TestServer;
+
+public static class TestServerFactory
+{
+  public static WebApplicationFactory<TProgram> CreateTestServer<TProgram>() where TProgram : class
+  {
+    return new WebApplicationFactory<TProgram>()
+      .WithWebHostBuilder(builder => builder
+        .UseSolutionRelativeContentRoot(Directory.GetCurrentDirectory()));
+  }
+}
--- src/ChromeProtocol.Runtime.Tests/WebSocketProtocolClientTests.cs
+++ src/ChromeProtocol.Runtime.Tests/WebSocketProtocolClientTests.cs
@@ -3,38 +3,28 @@ using ChromeProtocol.Runtime.Messaging.Extensions;
 using ChromeProtocol.Runtime.Tests.Protocol;
 using ChromeProtocol.Runtime.Tests.Protocol.TestServer;
 using ChromeProtocol.Tests.Extensions.Logging;
-using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
 using Xunit.Abstractions;
 
 namespace ChromeProtocol.Runtime.Tests;
 
-public class WebSocketProtocolClientTests : IClassFixture<TestServerFixture>, IAsyncLifetime
+public class WebSocketProtocolClientTests : IAsyncLifetime
 {
-  private readonly WebApplicationFactory<Protocol.TestServer.Program> _serverFactory;
   private readonly ILogger _clientLogger;
-  private IProtocolClient _protocolClient;
-  private CancellationTokenSource _tokenSource = new CancellationTokenSource(30_000);
-  private CancellationToken Token => _tokenSource.Token;
 
-  public WebSocketProtocolClientTests(TestServerFixture serverFixture, ITestOutputHelper testOutputHelper)
+  public WebSocketProtocolClientTests(ITestOutputHelper testOutputHelper)
   {
-    _serverFactory = serverFixture.ServerFactory;
     _clientLogger = new XunitLogger(testOutputHelper, "Client");
-  }
-
-  public async Task InitializeAsync()
-  {
-    var nativeClient = _serverFactory.Server.CreateWebSocketClient();
-    _protocolClient = await TestHostProtocolClient.CreateAsync(nativeClient, new Uri(_serverFactory.Server.BaseAddress, "/ws"), _clientLogger, Token);
-    _clientLogger.LogInformation("Connecting to the test WebSocket server...");
-    await _protocolClient.ConnectAsync(Token).ConfigureAwait(false);
-    _clientLogger.LogInformation("Connected successfully");
   }
 
   [Fact]
   public async Task SendCommandAsync_Echo_ShouldSucceed()
   {
-    var result = await _protocolClient.SendCommandAsync(new EchoCommand("Hello")).ConfigureAwait(false);
+    var tokenSource = new CancellationTokenSource(30_0000);
+    await using var factory = TestServerFactory.CreateTestServer<Program>();
+    using var client = await CreateServerClient(factory.Server, tokenSource.Token).ConfigureAwait(false);
+
+    var result = await client.SendCommandAsync(new EchoCommand("Hello"), token: tokenSource.Token).ConfigureAwait(false);
 
     Assert.Equal("Hello", result.Message);
   }
@@ -42,9 +32,13 @@ public class WebSocketProtocolClientTests : IClassFixture<TestServerFixture>, IA
   [Fact]
   public async Task SendCommandAsync_AlwaysError_ShouldFail()
   {
+    var tokenSource = new CancellationTokenSource(30_0000);
+    await using var factory = TestServerFactory.CreateTestServer<Program>();
+    using var client = await CreateServerClient(factory.Server, tokenSource.Token).ConfigureAwait(false);
+
     try
     {
-      await _protocolClient.SendCommandAsync(new AlwaysErrorCommand(), token: Token).ConfigureAwait(false);
+      await client.SendCommandAsync(new AlwaysErrorCommand(), token: tokenSource.Token).ConfigureAwait(false);
     }
     catch (ProtocolErrorException e)
     {
@@ -56,27 +50,44 @@ public class WebSocketProtocolClientTests : IClassFixture<TestServerFixture>, IA
   [Fact]
   public async Task FireCommandAsync_TriggerEvent_ShouldSendEventBack()
   {
-    _serverFactory.Server.CreateWebSocketClient();
-    var tcs = new TaskCompletionSource<bool>();
-    Token.Register(() => tcs.TrySetCanceled(), useSynchronizationContext: false);
+    var tokenSource = new CancellationTokenSource(30_0000);
+    await using var factory = TestServerFactory.CreateTestServer<Program>();
+    using var client = await CreateServerClient(factory.Server, tokenSource.Token).ConfigureAwait(false);
 
-    var subscription = _protocolClient.SubscribeOnceAsync<EventTriggeredEvent>(async _ =>
+    var tcs = new TaskCompletionSource<bool>();
+    tokenSource.Token.Register(() => tcs.TrySetCanceled(), useSynchronizationContext: false);
+
+    var subscription = client.SubscribeOnceAsync<EventTriggeredEvent>(async _ =>
     {
       tcs.TrySetResult(true);
     });
 
-    Token.Register(() => subscription.Dispose());
+    tokenSource.Token.Register(() => subscription.Dispose());
 
-    await _protocolClient.FireCommandAsync(new TriggerEventCommand(), token: Token).ConfigureAwait(false);
+    await client.FireCommandAsync(new TriggerEventCommand(), token: tokenSource.Token).ConfigureAwait(false);
     var result = await tcs.Task.ConfigureAwait(false);
 
     Assert.True(result);
   }
 
+  public async Task InitializeAsync()
+  {
+    _clientLogger.LogInformation("Start");
+  }
+
   public async Task DisposeAsync()
   {
-    _tokenSource.Cancel();
     _clientLogger.LogInformation("Shutdown");
-    _protocolClient.Dispose();
+  }
+
+  private async Task<IProtocolClient> CreateServerClient(TestServer server, CancellationToken token = default)
+  {
+    _clientLogger.LogInformation("Connecting to the test WebSocket server...");
+    var nativeClient = server.CreateWebSocketClient();
+    var client = await TestHostProtocolClient.CreateAsync(nativeClient, new Uri(server.BaseAddress, "/ws"), _clientLogger, token).ConfigureAwait(false);
+    await client.ConnectAsync(token).ConfigureAwait(false);
+    _clientLogger.LogInformation("Connected successfully");
+
+    return client;
   }
 }
