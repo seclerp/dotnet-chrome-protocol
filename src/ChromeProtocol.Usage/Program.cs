811b6e0ee95f7a0f141e01163dbcdf2f3dce74b2
Andrii Rublov <seclerp.dev@gmail.com>
2025-01-11 22:05:04 +0100

    Samples: Add basic example draft

---
+++ src/ChromeProtocol.Usage/Program.cs
@@ -0,0 +1,46 @@
+﻿using ChromeProtocol.Domains;
+using ChromeProtocol.Runtime.Browser;
+using ChromeProtocol.Runtime.Messaging.Logging;
+using ChromeProtocol.Runtime.Messaging.WebSockets;
+using ChromeProtocol.Usage;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Console = System.Console;
+
+const string PageUrl = "https://google.com";
+
+using var chromium = await ChromiumLauncher.Create()
+  .WithUserProfileDirectory("D:\\TestCookies")//Directory.CreateTempSubdirectory("ChromiumUserData").FullName)
+  .WithRemoteDebuggingPort(0)
+  .WithArguments(
+    "--no-default-browser-check",
+    "--no-first-run",
+    "--disable-extensions",
+    "--disable-popup-blocking ",
+    "--disable-translate",
+    "--disable-background-networking",
+    "--disable-sync",
+    "--disable-background-timer-throttling",
+    "--disable-infobars",
+    "--disable-component-extensions-with-background-pages"
+  )
+  .WithPage(PageUrl)
+  .LaunchLocalAsync(@"C:\Program Files\Google\Chrome\Application\chrome.exe");
+
+using var browserConnection = new DefaultProtocolClient(chromium.DebuggingEndpoint, NullLogger.Instance);
+var clientLogger =
+  new DefaultProtocolClientLogger(browserConnection, new SimpleConsoleLogger(nameof(Program), LogLevel.Trace));
+clientLogger.StartLogging();
+
+await browserConnection.ConnectAsync();
+var pages = await browserConnection.SendCommandAsync(Target.GetTargets());
+var page = pages.TargetInfos.FirstOrDefault(target => target.Type == "page");
+
+var attachResult = await browserConnection.SendCommandAsync(Target.AttachToTarget(page.TargetId, Flatten: true));
+using var pageConnection = browserConnection.CreateScoped(attachResult.SessionId.Value);
+
+var result = await pageConnection.SendCommandAsync(Network.GetCookies([PageUrl]));
+
+Console.WriteLine(chromium.DebuggingEndpoint);
+
+
---
+++ src/ChromeProtocol.Usage/SimpleConsoleLogger.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace ChromeProtocol.Usage;
+
+public class SimpleConsoleLogger : ILogger
+{
+  private readonly string _name;
+  private readonly LogLevel _minLogLevel;
+
+  public SimpleConsoleLogger(string name, LogLevel minLogLevel = LogLevel.Information)
+  {
+    _name = name;
+    _minLogLevel = minLogLevel;
+  }
+
+  public IDisposable BeginScope<TState>(TState state) => null;
+
+  public bool IsEnabled(LogLevel logLevel) => logLevel >= _minLogLevel;
+
+  public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+  {
+    if (!IsEnabled(logLevel))
+    {
+      return;
+    }
+
+    if (formatter == null)
+    {
+      throw new ArgumentNullException(nameof(formatter));
+    }
+
+    string message = formatter(state, exception);
+
+    if (string.IsNullOrEmpty(message))
+    {
+      return;
+    }
+
+    Console.WriteLine($"{DateTime.Now:o}: [{logLevel}] {_name} - {message}");
+
+    if (exception != null)
+    {
+      Console.WriteLine(exception);
+    }
+  }
+}
